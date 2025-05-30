4323f896f49eda3132170122116538270c2ef03d
MR. AMIR <136250878+amirfarebii34310@users.noreply.github.com>
2025-05-30 12:11:13 +0500

    Update time.js

--- Priyansh/commands/time.js
+++ Priyansh/commands/time.js
@@ -1,29 +1,33 @@
+/**
+* @author ProCoderMew
+* @warn Do not edit code or edit credits
+*/
+
 module.exports.config = {
-  name: "time",
-  version: "1.0.0",
-  hasPermssion: 0,
-  credits: "Secret",
-  description: "( ????? ???? & ???? )",
-  commandCategory: "Time and Date",
-  usages: "( Exact time )",
-  cooldowns: 0,
-  dependencies: []
+	name: "simteach",
+	version: "2.0.6",
+	hasPermssion: 0,
+	credits: "ProCoderMew",
+	description: "Dáº¡y bot (dÃ¹ng cho lá»‡nh sim)",
+	commandCategory: "general",
+	usages: "hello => goodbye",
+	cooldowns: 5,
+	dependencies: {
+		"axios": ""
+	},
+    envConfig: {
+        APIKEY: ""
+    }
 };
 
-module.exports.run = async function ({ api, event, args, Currencies, Users }) {
-  const moment = require("moment-timezone");
-  var supremo = moment.tz('Asia/Karachi').format('hh:mm:ss');
-  var draven = moment.tz('Asia/Manila').format('D/MM/YYYY');
-  var kiel = moment.tz('Asia/Manila').format('dddd');
-  if (kiel == 'Sunday') kiel = 'Sunday'
-  if (kiel == 'Monday') kiel = 'Monday'
-  if (kiel == 'Tuesday') kiel = 'Tuesday'
-  if (kiel == 'Wednesday') kiel = 'Wednesday'
-  if (kiel == "Thursday") kiel = 'Thursday'
-  if (kiel == 'Friday') kiel = 'Friday'
-  if (kiel == 'Saturday') kiel = 'Saturday'
-  let name = await Users.getNameUser(event.senderID);
-  return api.sendMessage(`?©¤©¤©¤©¤©¤ ?¡º ???? ¡»? ©¤©¤©¤©¤©¤?\n?????¡¸©z${name}©{¡¹\n??? ??????? ???? : ${supremo} \n??? : ${draven} (${kiel})\n?©¤©¤©¤©¤©¤ ?¡º ???? ¡»? ©¤©¤©¤©¤©¤?`, event.threadID, event.messageID)
+module.exports.run = async function({ api, event, args }) {
+	const { APIKEY } = global.configModule.teach;
+	const axios = global.nodemodule["axios"];
+	const { data } = await axios("https://meewmeew.info/simsimi/teach?apikey=" + APIKEY, {
+		method: "POST",
+		headers: { "Content-Type": "application/json" },
+		data: { teach: args.join(" ") }
+	});
+	if (data.success == false) return api.sendMessage(`${data.error}`, event.threadID, event.messageID);
+	return api.sendMessage(`${data.data}`, event.threadID, event.messageID);
 }
-
-    

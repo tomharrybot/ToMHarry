22baf6de59192ef5a2be0e331877612fb1f652f6
tomharrybot <tomharrybott@gmail.com>
2025-06-01 00:36:03 +0500

    Update uid.js

--- Priyansh/commands/uid.js
+++ Priyansh/commands/uid.js
@@ -1,5 +1,5 @@
     module.exports.config = {
-	name: "uid",
+	name: "uidv2",
 	version: "1.0.0",
 	hasPermssion: 0,
 	credits: "Mirai Team",
@@ -9,9 +9,9 @@
 };
 
 module.exports.run = function({ api, event }) {
-	if (Object.keys(event.mentions) == 0) return api.shareContact(`${event.senderID}`, event.threadID, event.messageID);
+	if (Object.keys(event.mentions) == 0) return api.sendMessage(`${event.senderID}`, event.threadID, event.messageID);
 	else {
-		for (var i = 0; i < Object.keys(event.mentions).length; i++) api.shareContact(`${Object.values(event.mentions)[i].replace('@', '')}: ${Object.keys(event.mentions)[i]}`, event.threadID);
+		for (var i = 0; i < Object.keys(event.mentions).length; i++) api.sendMessage(`${Object.values(event.mentions)[i].replace('@', '')}: ${Object.keys(event.mentions)[i]}`, event.threadID);
 		return;
 	}
 }
